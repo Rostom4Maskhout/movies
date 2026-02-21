503a00831cc4ad9e2998d5c511e596d6e3edd438
yyousef348 <sobhyyousef348@gmail.com>
2026-02-21 22:17:39 +0200

    feat: integrate login screen into main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/screens/login_screen.dart'; // استدعي الشاشة هنا
+import 'package:movies_app/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
